c1d6a8f7efad9bc97fdd08fb10e9b5eaeb32e5d6
wild-mouse <wildmouse0813@gmail.com>
2021-08-13 20:37:31 +0900

    Update base token info

--- contracts/Uni.sol
+++ contracts/Uni.sol
@@ -5,16 +5,16 @@ import "./SafeMath.sol";
 
 contract Uni {
     /// @notice EIP-20 token name for this token
-    string public constant name = "Uniswap";
+    string public constant name = "MileShare";
 
     /// @notice EIP-20 token symbol for this token
-    string public constant symbol = "UNI";
+    string public constant symbol = "MILE";
 
     /// @notice EIP-20 token decimals for this token
     uint8 public constant decimals = 18;
 
     /// @notice Total number of tokens in circulation
-    uint public totalSupply = 1_000_000_000e18; // 1 billion Uni
+    uint public totalSupply = 100_000_000e18; // 1 billion Uni
 
     /// @notice Address which may mint new tokens
     address public minter;
