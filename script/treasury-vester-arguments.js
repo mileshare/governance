1794c597cdf8337f4f0fbfc63e3cb302ffa56f26
wild-mouse <wildmouse0813@gmail.com>
2021-09-28 15:11:11 +0900

    Add arguments

---
+++ script/treasury-vester-arguments.js
@@ -0,0 +1,8 @@
+module.exports = [
+  process.env.TREASURY_VERSTER_TOKEN_ADDRESS,
+  process.env.TREASURY_VERSTER_RECIPIENT,
+  process.env.TREASURY_VERSTER_AMOUNT,
+  process.env.TREASURY_VERSTER_BEGIN,
+  process.env.TREASURY_VERSTER_CLIFF,
+  process.env.TREASURY_VERSTER_END
+];
